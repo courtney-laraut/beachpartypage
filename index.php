32708ac07ea696adf24cf1a03cee505d45274191
Courtney <126120063+courtney-laraut@users.noreply.github.com>
2023-06-17 23:06:28 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+    <link rel="stylesheet" type="text/css" media='screen and (max-width: 360px)' href="mobile.css">
+    <link rel='stylesheet' type='text/css' media='screen and (min-width: 361px)' href='desktop.css'>
+
+
+    <script src='main.js'></script>
+    <style>
+        body {
+            background-image: url("images/beach.jpg");
+            background-position: top left;
+            background-size: 100%;
+            background-repeat: no-repeat;
+        }
+    </style>
+</head>
+<body>
+
+<!-- Top Image -->
+    <div id="mainhead">
+
+    <h2 id='bigheadreg'>END OF</h2>
+    <h2 id='bigheadreg'>SCHOOL</h2>
+    <h2 id='bighead'>BEACH</h2>
+    <h2 id='bighead'>PARTY</h2>
+            
+    </div>
+
+    <hr id="firstline">
+
+    <!-- Blurb of Invitation -->
+
+    <div id="blurb">
+    <p id="regtext">Join us for a day in the sun as we say:</p>
+            <blockquote id="styletext"><i>Goodbye</i>, <span style="font-family: lilita; font-size: 1.5em">SCHOOL!</span> <i>Hello</i>, <span style="font-family: lilita; font-size: 1.5em">SUMMER!</span></blockquote>
+    </div>
+
+    <hr id="secondline">
+
+    <div id="detail">
+        <h2>When and Where</h2>
+        <img id="beachpic" src="images/vecteezy_beach-scene-watercolor-clipart-ai-generated_24065327_316.png" alt="">
+            <p>On thursday ( June 22) afternoon, we will be transforming our side field into a tropical luau. Once kids arrive at Go-Go from school, the fun begins immediately!</p>
+        <h3>Fun and Games</h3>
+
+            <p>Musical beach towels, volley beach ball, pool noodle limbo, and more! Of course, we will also be breaking out the water toys to keep everyone feeling cool and refreshed.</p>
+
+        <h3>It's Hot Out There!</h3>
+        <img id="flowerpic" src=images\vecteezy_flamingo-swimming-float-isolated_23403295_589.png alt="">
+
+            <p>A hydration station with cold water will be available along with plenty of shade under some beach umbrellas! </p>
+    </div>
+    <hr id="secondline">
+
+    <div class="randotext"><img class="inlinePic" src="images/keepcool.jpg" alt=""><p class="centered">Schedule</p> </div>
+<hr id="secondline">
+
+    <div id=detail>
+        
+            <p>Here's a sneak preview at our planned activities:</p>
+<table>
+    <tr>
+        <td class="time">1:50 - Party Starts </td>
+    </tr>
+    <tr>
+        <td class="info">We will all be outside on the field as the festivities kick off!</td>
+    </tr>
+    <tr>
+        <td class="time">2:15 - Hot Beach Ball</td>
+    </tr>
+    <tr>
+        <td class="info">Don't be the last one holding the beach ball when the music turns off!</td>
+
+    </tr>
+    <tr>
+        <td class="time">2:50 - Pool Noodle Limbo</td>
+    </tr>
+    <tr>
+    <td class="info">How low can you go?</td>
+
+    </tr>
+    <tr>
+        <td class="time">3:25 - Hula Hoop Relay</td>
+    </tr>
+    <tr>
+    <td class="info">Which team can pass the hula hoop from one end of their chain to the other?</td>
+
+    </tr>
+    <tr>
+        <td class="time">4:00 - Musical Beach Towels</td>
+    </tr>
+    <tr>
+    <td class="info">Just like musical chairs, but you get to flop onto beach towels instead!</td>
+
+    </tr>
+    <tr>
+        <td class="time">4:35 - Water Palooza!</td>
+    </tr>
+    <tr>
+    <td class="info">All water toys, all fun.</td>
+
+    </tr>
+</table>
+
+<br>
+
+
+
+     <div class="floattext"><p class="didyou">Did you know? There will be prizes for all of the above activies!</p></div>
+     
+
+    <p>In between all scheduled activities we will have free time where children are welcome to participate in volley beach ball, kite flying, and bubble blowing. <img class="floralimg" src="images\vecteezy_tropical-flower-composition-watercolor-hand-paint_10335964_66.png" alt=""></p>
+    <br>
+
+    <h3 class="hidden">s </h3>
+    </div>
+    
+    <hr id="secondline">
+
+<div class="randotext"><img class="inlinePic" src="images/strawhat.jpg" alt=""> <p class="centered1">See You There!</p></div>
+<hr id="secondline">
+
+
+</div>
+</body>
+
+<footer>
+    Created by Courtney LaRaut
+</footer>
+</html>
